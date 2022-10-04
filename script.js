f8c69795af9305bde35e1a54eb7e62865b34e529
Pop Mihai Adrian <112785493+popmihaiadrianwebdevelopment@users.noreply.github.com>
2022-10-04 12:44:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,24 @@
-const form = document.getElementById('form');
-const email = document.getElementById('email');
-const button = document.getElementById('button');
+const msg = 'Please provide a valid email address';
+const form = document.querySelector('form');
+const button = form.querySelector('[type="submit"]');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    var emailValue = email.value.trim();
-    var pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    
-    if(emailValue === ''){
-        errorFunc(email, 'Email cannot be empty')
-      }else if (!emailValue.match(pattern)){
-        errorFunc(email, 'Email is not valid')
-      }
-      else {
-        successFunc(email)
-      }
-})
+const error = document.createElement('p');
+error.classList.add('error_text');
+error.textContent = msg;
 
-function errorFunc(req, message){
-    const formControl = req.parentElement;
-    const span = formControl.querySelector('span');
-    span.innerText = message;
-    req.className += 'error';
-    span.className += 'error-text';
+function checkEmail (e) {
+	e.preventDefault();
+	const email = e.target.querySelector('[type="email"]').value;
+	if (!validateEmail(email)) {
+		form.classList.add('error');
+		form.insertBefore(error, button);
+	} else {
+		form.removeChild(error);
+		form.classList.remove('error');
+	}
 }
-
-function successFunc(req) {
-    req.className += 'success';
+function validateEmail (email) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
-
-// function validate() {
-//     const emailValue = email.target.trim()
-
-//     if (emailValuie = '') {
-//         setErrorFor (email, 'Email cannot be empty');
-//     }else if (!isEmail(emailValue)) {
-//         setErrorFor (email, 'Please provide a valid email');
-//     } else {
-//         setSuccessFor (email)
-//     }
-// }
-
-// function setErrorFor() {
-
-// }
-
-// function setSuccesFor() {
-
-// }
-
-// function isEmail(email) {
-//     return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test (
-//         email)
-    
-// }
+form.addEventListener('submit', checkEmail);
